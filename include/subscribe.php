03089ee5831166783e9659cb5f355a37452186ca
pasha2023 <pasha.n@rajyaseva.in>
2023-06-22 19:49:34 +0530

    Mailchimp API key remored

--- include/subscribe.php
+++ include/subscribe.php
@@ -1,7 +1,7 @@
 <?php
 
-$apiKey = '2242fd66cbd184015aa9830d557c0dac-us17'; // Your MailChimp API Key
-$listId = '765f38370d'; // Your MailChimp List ID
+$apiKey = ''; // Your MailChimp API Key
+$listId = ''; // Your MailChimp List ID
 
 if( isset( $_GET['list'] ) AND $_GET['list'] != '' ) {
 	$listId = $_GET['list'];
